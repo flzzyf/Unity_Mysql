c557ec2ce6fe559139d26505d9c8030271dd1f28
Flzzyf <flzzyf076@gmail.com>
2018-08-06 21:54:11 +0800

    能用但是报错，随便了

--- Test_Mysql/Assets/MysqlConnection.cs
+++ Test_Mysql/Assets/MysqlConnection.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using MySql.Data;
 using MySql.Data.MySqlClient;
+using System.Data;
 
 public class MysqlConnection : MonoBehaviour
 {
@@ -12,19 +11,20 @@ public class MysqlConnection : MonoBehaviour
     static string password = "";
     static string database = "sa";
 
-    public MySql.Data.MySqlClient.MySqlConnection conn;
+    public MySqlConnection conn;
 
     void Start()
     {
-        // Server=localhost;user id=root;password=localhost;Database=web;Port=3306;charset=utf8;
-
+        host = "184.170.221.45";
+        database = "lemon";
+        password = "hesoyam";
         string connectionString = string.Format("Server={0};Database={1};user id={2};password={3}",
         host, database, username, password);
 
         conn = new MySql.Data.MySqlClient.MySqlConnection(connectionString);
         conn.Open();
 
-        string query = "select * from lemon";
+        string query = "select * from juice";
         MySql.Data.MySqlClient.MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(query, conn);
         command.ExecuteNonQuery();
 
@@ -35,9 +35,4 @@ public class MysqlConnection : MonoBehaviour
         }
     }
 
-
-    void Update()
-    {
-
-    }
 }
