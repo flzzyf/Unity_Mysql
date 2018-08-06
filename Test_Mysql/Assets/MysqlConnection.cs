622eaddccce41b7212d56254dd9ce91ff487523d
flzzyf <2981019122@qq.com>
2018-08-06 20:51:17 +0800

    Init

---
+++ Test_Mysql/Assets/MysqlConnection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+public class MysqlConnection : MonoBehaviour
+{
+    static string host = "localhost";
+    static int port = 3306;
+    static string username = "root";
+    static string password = "";
+    static string database = "sa";
+
+    public MySql.Data.MySqlClient.MySqlConnection conn;
+
+    void Start()
+    {
+        // Server=localhost;user id=root;password=localhost;Database=web;Port=3306;charset=utf8;
+
+        string connectionString = string.Format("Server={0};Database={1};user id={2};password={3}",
+        host, database, username, password);
+
+        conn = new MySql.Data.MySqlClient.MySqlConnection(connectionString);
+        conn.Open();
+
+        string query = "select * from lemon";
+        MySql.Data.MySqlClient.MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(query, conn);
+        command.ExecuteNonQuery();
+
+        MySqlDataReader myDataReader = command.ExecuteReader();
+        while (myDataReader.Read() == true)
+        {
+            Debug.Log(myDataReader["name"]);
+        }
+    }
+
+
+    void Update()
+    {
+
+    }
+}
